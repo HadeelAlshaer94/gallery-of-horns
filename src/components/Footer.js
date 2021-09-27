d8448435390975c44290b804bbcbd6329dedf9ee
HadeelAlshaer94 <Hadeelalshaer94@outlook.com>
2021-09-27 08:28:10 +0300

    finished lab01

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer>HornedBeast 2021</footer>
+        )
+    }
+}
+
+export default Footer;
